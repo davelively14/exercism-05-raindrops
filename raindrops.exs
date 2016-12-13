8fa3f55bc89acb84f614688f35a48f43bc7b6621
Dave Lively <davelively14@gmail.com>
2016-12-13 18:54:14 -0500

    Complete

--- raindrops.exs
+++ raindrops.exs
@@ -8,8 +8,25 @@ defmodule Raindrops do
   - If the number does not contain 3, 5, or 7 as a prime factor,
     just pass the number's digits straight through.
   """
-  @spec convert(pos_integer) :: String.t
-  def convert(number) do
+  @factors [{3, "Pling"}, {5, "Plang"}, {7, "Plong"}]
 
+  @spec convert(pos_integer) :: String.t
+  def convert(number, factors \\ @factors), do: check_all(number, factors, "")
+
+  def check_all(number, [], result) do
+      if result == "", do: Integer.to_string(number), else: result
   end
+
+  def check_all(number, [{prime, string} | tail], result) do
+    if rem(number, prime) == 0 do
+      check_all(number, tail, result <> string)
+    else
+      check_all(number, tail, result)
+    end
+  end
+
+  # when rem(number, 3) == 0, do: "Pling"
+  # def convert(number) when rem(number, 5) == 0, do: "Plang"
+  # def convert(number) when rem(number, 7) == 0, do: "Plong"
+  # def convert(number), do: number
 end
